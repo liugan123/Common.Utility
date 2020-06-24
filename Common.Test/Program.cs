015cab5c4ea66d0023e424b62c9aa070e95a9976
lg <184932840@qq.com>
2020-06-24 15:56:36 +0800

    添加文件帮助类---压缩文件

---
+++ Common.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common.Utility;
+using System;
+
+namespace Common.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FileHelper.ZipFile());
+        }
+    }
+}
---
+++ Common.Utility/FileHelper.cs
@@ -0,0 +1,47 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common.Utility
+{
+    /// <summary>
+    /// 文件相关的帮助类
+    /// </summary>
+    public static class FileHelper
+    {
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="filenames"></param>
+        /// <returns></returns>
+        public static bool ZipFile(params string[] filenames)
+        {
+            //需要安装DotNetZip
+            try
+            {
+                ZipFile zip = new ZipFile(Encoding.UTF8);
+                zip.AddDirectory(@"E:\Code\CSharp-Attribute","");
+                //MemoryStream memoryStream = new MemoryStream();
+                FileStream fs = new FileStream("测试.zip", FileMode.Create);
+                //zip.Save(memoryStream);
+                zip.Save(fs);
+
+                //memoryStream.WriteTo(fs);
+                //memoryStream.Close();
+                fs.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static bool SaveFile(Stream stream)
+        {
+            return true;
+        }
+    }
+}
